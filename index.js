ba00feef65438c371729bcdcb18ab390ce92624c
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-03-02 22:42:05 -0500

    Added functions

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ var spreadsheet_functions = {
     } else {
       return b;
     }
+  },
+  or: function(){
+    for( var i in arguments ){
+      if( arguments[i] ){
+        return true;
+      }
+    }
+    return false;
+  },
+  and: function(){
+    for( var i in arguments ){
+      if( ! arguments[i] ){
+        return false;
+      }
+    }
+    return true;
   }
 
 
