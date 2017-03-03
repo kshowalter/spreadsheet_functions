c4456cc870279adb3cedec661bd5b1c3e1fa7adc
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-03-02 22:28:43 -0500

    Init files.

---
+++ index.js
@@ -0,0 +1,16 @@
+var spreadsheet_functions = {
+  if: function(test, a, b){
+    if(test){
+      return a;
+    } else {
+      return b;
+    }
+  }
+
+
+
+};
+
+
+
+module.exports = spreadsheet_functions;
